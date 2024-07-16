ab04e96201958db95848f3585a079deee6832107
Alexandr Shin <a.shin@faberlic.com>
2024-07-16 17:55:30 +0300

    upd

--- src/blocks/components/products/products.js
+++
@@ -1,7 +0,0 @@
-function productsAmountSet(){
-    var products__item = $('.products__item');
-    if (products__item.length){
-        products__item.parent().addClass('products--' + products__item.length);
-    }
-}
-productsAmountSet();
